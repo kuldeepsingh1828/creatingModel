7a4ecad39f1b8da0dc5c0a5745cbef81ca9fcc5e
kuldeepsingh1828@gmail.com <kuldeepsingh1828@gmail.com>
2020-10-03 12:08:37 +0530

    book update and delete

--- book/urls.py
+++ book/urls.py
@@ -3,5 +3,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('update/<int:id>', views.updateBook, name='update'),
+    path('delete/<int:id>', views.deleteBook, name='update'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Book2
 
 
 def index(request):
     books = Book2.objects.all()
-    return  render(request, 'book/books.html', {'books': books})
+    return render(request, 'book/books.html', {'books': books})
+
+
+def deleteBook(request,id):
+    Book2.objects.filter(pk=id).delete()
+    return redirect('/book')
+
+
+def updateBook(request,id):
+    book = Book2.objects.filter(id=id)
+    if request.method == 'POST':
+        book = Book2.objects.get(pk=id)
+        book.book_Name = request.POST['book_Name']
+        book.author_Name = request.POST['author_Name']
+        if request.POST['pub_date']:
+            book.pub_date = request.POST['pub_date']
+        book.price = request.POST['price']
+        if request.FILES:
+            book.image = request.FILES['image']
+        book.save()
+        return redirect('/book')
+    else:
+        return render(request, 'book/update.html', {'book': book})
